9ff9deaf2fc20848bc9e498f6fa71c1c0650b512
Mohit Kumar <64537818+mohitktanwr@users.noreply.github.com>
2021-05-23 13:20:56 +0530

    Update resnet.py

--- resnest/torch/models/resnet.py
+++ resnest/torch/models/resnet.py
@@ -173,7 +173,7 @@ class ResNet(nn.Module):
         self.cardinality = groups
         self.bottleneck_width = bottleneck_width
         # ResNet-D params
-        self.inplanes = stem_width*2 if deep_stem else 64
+        self.inplanes = stem_width*2 if deep_stem else 512
         self.avg_down = avg_down
         self.last_gamma = last_gamma
         # ResNeSt params
@@ -206,8 +206,8 @@ class ResNet(nn.Module):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 512, layers[0], norm_layer=norm_layer, is_first=False)
-        self.layer2 = self._make_layer(block, 256, layers[1], stride=2, norm_layer=norm_layer)
+        self.layer1 = self._make_layer(block, 1024, layers[0], norm_layer=norm_layer, is_first=False)
+        self.layer2 = self._make_layer(block, 512, layers[1], stride=2, norm_layer=norm_layer)
         if dilated or dilation == 4:
             self.layer3 = self._make_layer(block, 256, layers[2], stride=1,
                                            dilation=2, norm_layer=norm_layer,
@@ -223,10 +223,10 @@ class ResNet(nn.Module):
                                            dilation=2, norm_layer=norm_layer,
                                            dropblock_prob=dropblock_prob)
         else:
-            self.layer3 = self._make_layer(block, 128, layers[2], stride=2, #check
+            self.layer3 = self._make_layer(block, 256, layers[2], stride=2, #check
                                            norm_layer=norm_layer,
                                            dropblock_prob=dropblock_prob)
-            self.layer4 = self._make_layer(block, 64, layers[3], stride=2,
+            self.layer4 = self._make_layer(block, 256, layers[3], stride=2,
                                            norm_layer=norm_layer,
                                            dropblock_prob=dropblock_prob)
         self.avgpool = GlobalAvgPool2d()
